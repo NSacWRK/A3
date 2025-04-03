ef539024c21291ff8a845cd263c8af6e7b9490e6
VickramGill <vickramgill16@gmail.com>
2025-04-03 19:51:46 +0000

    countEntries Function Implementation

--- A3/DB.c
+++ A3/DB.c
@@ -26,8 +26,80 @@ void exportDB(char *filename){
 // Count Entries (Currently Empty Function)
 // This function takes the name of a member of the picniTable entry and a value for that member as paramter and returns 
 // the number of entries in the picnicTable which have that value in the memberName
+
+/*The function must be implemented for:
+ *   1- Table Type
+ *   2- Surface Material
+ *   3- Structural Material
+ *   4 - Neghborhood ID
+ *   4- Neighborhood Name
+ *   5- Ward
+ */
 int countEntries(char *memberName, char * value){
-    return 0; // Return for now - as we're creating empty functions (will be worked on later) for MS1
+    //return 0; // Return for now - as we're creating empty functions (will be worked on later) for MS1
+    int i,j,nbhdID,entryCount;
+    entryCount = 0;
+
+    if(Db == NULL || Db->picnicTableTable == NULL){
+        printf("Error - No Database Available, or No Elements Within PicnicTable Available");
+        return 0;
+    }
+
+    for (i = 0; i < Db->picnicTableTable->picnicT_ElementCount; i++){
+        PicnicTableEntry picTEntry;
+        picTEntry = Db->picnicTableTable->picnicT_entries[i];
+
+        // #1 Table Type (UNDER TABLE STRUCT - (Look at CMPT_201_Database_Description PDF - Table Relationships))
+        if(strcmp(memberName, "Table Type") == 0){
+            for(j=0; j < Db->tableTypeTable->tableT_ElementCount; j++){
+                if(Db->tableTypeTable->tableT_entries[j].code == picTEntry.tableTypeId && strcmp(Db->tableTypeTable->tableT_entries[j].value, value) == 0){
+                    entryCount++;
+                }
+                
+            }
+        }
+        // #2) Surface Material (UNDER TABLE STRUCT -(Look at CMPT_201_Database_Description PDF - Table Relationships))
+        else if (strcmp(memberName, "Surface Material") == 0){
+            for(j=0; j < Db->surfaceMaterialTable->tableT_ElementCount; j++){
+                if(Db->surfaceMaterialTable->tableT_entries[j].code == picTEntry.surfaceMaterialId && strcmp(Db->surfaceMaterialTable->tableT_entries[j].value, value) == 0){
+                    entryCount++;
+                }
+            }
+        }
+        // #3) Structural Material (UNDER TABLE STRUCT - (Look at CMPT_201_Database_Description PDF - Table Relationships))
+        else if (strcmp(memberName, "Structural Material") == 0){
+            for(j=0; j < Db->structuralMaterialTable->tableT_ElementCount; j++){
+                if(Db->structuralMaterialTable->tableT_entries[j].code == picTEntry.structuralMaterialId && strcmp(Db->structuralMaterialTable->tableT_entries[j].value, value) == 0){
+                    entryCount++;
+                }
+            }
+        }
+
+        // # 4) Neighbourhood ID (Look at CMPT_201_Database_Description PDF - Table Relationships)
+        else if (strcmp(memberName, "Neighbourhood ID") == 0){
+            nbhdID = atoi(value);
+            if(picTEntry.neighbhdId == nbhdID){
+                entryCount++;
+            }
+        }
+
+        // #5) Neighbourhood Name (NEIGHBOURHOOD STRUCT - (Look at CMPT_201_Database_Description PDF - Table Relationships))
+        else if (strcmp(memberName, "Neighbourhood Name") == 0){
+            for(j=0; j < Db->neighborhoodTable->neighbourhoodT_ElementCount; j++){
+                if(Db->neighborhoodTable->neighbourhoodT_entries[j].code == picTEntry.neighbhdId && strcmp(Db->neighborhoodTable->neighbourhoodT_entries[j].value, value) == 0){
+                    entryCount++;
+                }
+            }
+        }
+
+        // #6) Ward (Within PicnicTable / picTEntry directly)
+        else if (strcmp(memberName, "Ward") == 0){
+            if(strcmp(picTEntry.ward, value) == 0){
+                entryCount++;
+            }
+        }
+    }
+    return entryCount;
 }
 
 // SortByMember Function
