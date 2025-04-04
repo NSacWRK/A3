f9d735e9429cc4b39a245662044479d8ed2eb35f
VickramGill <vickramgill16@gmail.com>
2025-04-04 19:03:56 +0000

    Little Implementation of sortByNeighbourhood - Used Same Format as reportByWard but still needs Changes

--- A3/DB.c
+++ A3/DB.c
@@ -118,7 +118,108 @@ void editTableEntry(int tableID, char *memberName, char *value){
 
 // this function produces a listing of picnic tables grouped by neighbourhoods in ascending alphabetical order
 void reportByNeighbourhood(){
-    return; // Return for now - as we're creating empty functions (will be worked on later) for MS1
+    int i, j, k;
+    char *wardArray[64];
+    char *currentNeighbourName, *tmpNeighbhd;
+    int neighAddedCounter;
+    int picnicTCount, diffNeighCount, tableTEC, surfaceMEC, structuralEC, neighEC;
+    currentNeighbourName = NULL;
+
+    picnicTCount = Db->picnicTableTable->picnicT_ElementCount;
+    diffNeighCountCount = 0; // distinct / different wards
+
+    if(Db == NULL || Db->picnicTableTable == NULL){
+        printf("Error - No Database Available, or No Elements Within PicnicTable Available");
+        return 0;
+    }
+
+    for(i=0; i < picnicTCount; i++){
+        currentNeighbourName = Db->picnicTableTable->picnicT_entries[i].neighbhdId;
+         wardAddedCounter = 0; //already seen this ward (alreadyadded)
+
+         for(k=0; k < diffWardCount; j++){
+            if(strcmp(wardArray[j], currentWard) == 0){
+                wardAddedCounter = 1;
+                break;
+            }
+         }
+
+         if(wardAddedCounter == 0){
+            wardArray[diffWardCount++] = currentWard;
+         }
+
+    }
+
+    for(i=0; i < diffWardCount; i++){
+        for(j=0; j < diffWardCount; j++){
+            if(strcmp(wardArray[i], wardArray[j]) < 0){
+                tmpWard = wardArray[i];
+                wardArray[i] = wardArray[j];
+                wardArray[j] = tmpWard;
+
+            }
+        }
+    }
+
+    for(i=0; i < diffWardCount; i++){
+        printf("\n%s\n", wardArray[i]);
+        printf("************************************************\n")
+
+        for(j=0; j < picnicTCount; j++){
+            PicnicTableEntry picTE;
+            picTE = Db->picnicTableTable->picnicT_entries[j];
+
+            if(strcmp(picTE.ward, wardArray[i]) == 0){
+                // Since these are integer values, PicnicTable doesnt store full names like "Wood" - 
+                // It Uses codes / ID's that are associated with certain values that link to the lookup tables (TableType, SurfaceMat, StructMat, Neigh...)
+                // Called Foreign Keys - ID"s that refer to entries in lookup Tables
+
+                char *tableT; // Lookup Table (Watch Video Once Again To Provide Explaination)
+                char *surfaceMat;// // Lookup Table (Watch Video Once Again To Provide Explaination)
+                char *structuralMat; //Lookup Table (Watch Video Once Again To Provide Explaination)
+                char *neighbourhoodName; // Lookup Table (Watch Video Once Again To Provide Explaination)
+
+                // Element Counts for 'For Loop' - just for clairty/visability sake
+                tableTEC = Db->tableTypeTable->tableT_ElementCount;
+                surfaceMEC = Db->surfaceMaterialTable->tableT_ElementCount;
+                structuralEC = Db->structuralMaterialTable->tableT_ElementCount;
+                neighEC = Db->neighborhoodTable->neighbourhoodT_ElementCount;
+
+                for (k=0; k < tableTEC; k++){
+                    if(Db->tableTypeTable->tableT_entries[k].code == picTE.tableTypeId){
+                        tableT = Db->tableTypeTable->tableT_entries[k].value;
+                        break;
+                    }
+                }
+
+                for(k=0; k < surfaceMEC; k++){
+                    if(Db->surfaceMaterialTable->tableT_entries[k].code == picTE.surfaceMaterialId){
+                        surfaceMat= Db->surfaceMaterialTable->tableT_entries[k].value;
+                        break;
+                    }
+                }
+
+                for(k=0; k < structuralEC; k++){
+                    if(Db->structuralMaterialTable->tableT_entries[k].code == picTE.structuralMaterialId){
+                        structuralMat = Db->structuralMaterialTable->tableT_entries[k].value;
+                        break;
+                    }
+                }
+
+                for(k=0; k < neighEC; k++){
+                    if(Db->neighborhoodTable->neighbourhoodT_entries[k].code == picTE.neighbhdId){
+                        neighbourhoodName = Db->neighborhoodTable->neighbourhoodT_entries[k].value;
+                        break;
+                    }
+                }
+
+                printf("%d     %s     %s     %s     %s     %d     %s     %s     %s", picTE.siteId, tableT, surfaceMat, structuralMat, picTE.streetAvenue, picTE.neighbhdId, neighbourhoodName, picTE.latitude, picTE.longitude);
+
+            }
+        }
+    }
+    return; 
+    //return; // Return for now - as we're creating empty functions (will be worked on later) for MS1
 }
 
 // this function produces a listing of picnic tables grouped by wards in ascending order
